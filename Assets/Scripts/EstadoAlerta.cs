fc5a292cb2e66e72ac4f7d70a1ab0fef313d3cd8
MaxiRava <maximoravazzani@gmail.com>
2023-04-29 16:43:56 -0300

    EstadoAlerta

--- Assets/Scripts/EstadoAlerta.cs
+++ Assets/Scripts/EstadoAlerta.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class EstadoAlerta : MonoBehaviour
 {
+    public float velocidadGiroBusqueda = 120f;
+
+    //busqueda
+    public float duracionBusqueda = 4f; 
+
+    private MaquinaDeEstados maquinaDeEstados;
+    private NavMesh navMesh;
+    private Vision vision;
+
+    //busqueda
+    private float tiempoBuscando;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        maquinaDeEstados = GetComponent<MaquinaDeEstados>();
+        navMesh = GetComponent<NavMesh>();
+        vision = GetComponent<Vision>();
+
+    }
+
+    void OnEnable()
+    {
+        navMesh.DetenerNMA();
+        tiempoBuscando = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        RaycastHit hit;
+        if (vision.verAlJugador(out hit))
+        {
+            navMesh.perseguirObjetivo = hit.transform;
+            maquinaDeEstados.ActivarEstado(maquinaDeEstados.EstadoPersecucion);
+            return;
+        }
+
+        transform.Rotate(0f, velocidadGiroBusqueda * Time.deltaTime, 0f);
+
+        //busqueda
+        tiempoBuscando += Time.deltaTime;
+        if(tiempoBuscando >= duracionBusqueda)
+        {
+            maquinaDeEstados.ActivarEstado(maquinaDeEstados.EstadoNormal);
+            return;
+        }
+
+
     }
 }
