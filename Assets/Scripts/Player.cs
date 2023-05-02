d8b16368aaf137309085f057100ac2e1ecf83513
joakobarros <joaquinbarroscs@gmail.com>
2023-05-02 18:21:02 -0300

    camara y movimiento de la pelota

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private new Rigidbody rigidbody;
+    public float velMovimiento;
+
+    public Vector2 sensibilidad;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      Movimiento();
+      Camara();
+    }
+
+    private void Movimiento()
+    {    
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        if (hor != 0 || ver != 0)
+        {
+            Vector3 direction = (transform.forward * ver + transform.right * hor).normalized;
+
+            rigidbody.velocity = direction * velMovimiento;
+        }
+    }
+
+    private void Camara()
+    {
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+
+        if (hor != 0)
+        {
+            transform.Rotate(0, hor * sensibilidad.x, 0);
+        }
     }
 }
