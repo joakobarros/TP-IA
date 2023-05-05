0dc7ca9faa57a09a5da9cc752c8cc3fde4aa74b3
joakobarros <joaquinbarroscs@gmail.com>
2023-05-05 14:14:53 -0300

    revision

--- Assets/Scripts/EstadoPaseo.cs
+++ Assets/Scripts/EstadoPaseo.cs
@@ -16,7 +16,7 @@ public class EstadoPaseo: MonoBehaviour
 
     void OnEnable()
     {
-        player.velMovimiento = 800;
+        player.velMovimiento = 1200;
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/EstadoPerseguido.cs
+++ Assets/Scripts/EstadoPerseguido.cs
@@ -16,7 +16,7 @@ public class EstadoPerseguido: MonoBehaviour
 
     void OnEnable()
     {
-        player.velMovimiento = 1500;
+        player.velMovimiento = 2000;
     }
 
     private void OnTriggerExit(Collider other)
